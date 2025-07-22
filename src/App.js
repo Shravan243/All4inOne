ea9f4bb19588cd876923f2fba7bfaf0f987ab0ba
Shravan <bangarwarshravan@gmail.com>
2025-07-22 20:06:50 +0530

    Made cahnges to slide-text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import "./App.css";
 const images = ["/img1.jpg", "/img2.jpg", "/img3.jpg", "/img4.jpg"];
 
 function App() {
+  const [scrollX, setScrollX] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollX(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const leftStyle = {
+    transform: `translateY(${-scrollX * 2}px)`,
+    opacity: Math.max(1 - scrollX / 500, -5),
+  };
+  const rightStyle = {
+    transform: `translateY(${scrollX * 2}px)`,
+    opacity: Math.max(1 - scrollX / 500, 0),
+  };
+
   const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
@@ -20,20 +37,29 @@ function App() {
         <img src="Menu icon-04.png" alt="menu" className="menu-icon" />
       </div>
 
-      <video autoPlay loop muted className="bg-video">
-        <source src="/A4op_Exhibition_New.mp4" type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video>
-
-      <div className="overlay">
+      <div className="video-holder">
+        <video autoPlay loop muted className="bg-video">
+          <source src="/A4op_Exhibition_New.mp4" type="video/mp4" />
+          Your browser does not support HTML5 video.
+        </video>
+        {/* <div className="overlay"> */}
         <div className="text-wrapper">
-          <h1 className="slide-left">All 4 One</h1>
-          <h1 className="slide-right">Planner</h1>
+          <h1 className="slide-left" style={leftStyle}>
+            All <span className="Hollow-text">4</span> One
+          </h1>
+          <h1 className="slide-right" style={rightStyle}>
+            Planner
+          </h1>
         </div>
+        {/* </div> */}
       </div>
-      {/* <div className="slideshow">
+
+      <div className="slideshow">
         <img src={images[currentImage]} alt="slideshow" />
-      </div> */}
+        <div className="vignette-overlay" />
+      </div>
+
+      <div className="footer"></div>
     </div>
   );
 }
